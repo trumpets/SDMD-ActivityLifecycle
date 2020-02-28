71bf8f6e0269a58ff0b8cb93ab0ac4d686429b71
Ivo Neskovic <ivo.neskovic@gmail.com>
2020-02-28 21:16:27 +0100

    Upgraded gradle and migrated to androidx.

--- app/src/main/java/gr/academic/city/sdmd/activitylifecycle/MainActivity.java
+++ app/src/main/java/gr/academic/city/sdmd/activitylifecycle/MainActivity.java
@@ -1,7 +1,7 @@
 package gr.academic.city.sdmd.activitylifecycle;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
